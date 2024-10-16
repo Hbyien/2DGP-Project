3ee9a8a263098f1476a9ee9ce288fdc84bbf33f1
Hyebin Lee <112963362+Hbyien@users.noreply.github.com>
2024-10-16 22:26:28 +0900

    오른쪽 왼쪽 이동

--- main.py
+++ main.py
@@ -4,29 +4,47 @@ from pico2d import *
 open_canvas()
 character_walk = load_image('image//walk.png')
 
-frame = 0
-running  = True
+
 
 def handle_events():
     global running
+    global  dir
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
 
-for x in range(0, 800, 10):
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+            elif event.key == SDLK_d:  #오른쪽이동
+                dir +=1
+            elif event.key == SDLK_a: #왼쪽이동
+                dir -=1
+
+        elif event.type ==SDL_KEYUP:
+            if event.key == SDLK_d:  #오른쪽이동
+                dir -=1
+            elif event.key == SDLK_a: #왼쪽이동
+                dir +=1
+
+#--------------------------------
+frame = 0
+running  = True
+x = 800//2
+dir = 0
+
+while running:
     clear_canvas()
     character_walk.clip_draw(frame*96, 0, 96, 94, x, 90)
     update_canvas()
 
     handle_events()
-    if not running:
-        break
 
-    frame = (frame+1) %8
+    frame = (frame+1) %6
+    x += dir*10
     delay(0.05)
 
 close_canvas()
