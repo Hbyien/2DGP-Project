499f103b01e3f0fc83a88fdf0bc5375547e55eb4
Hyebin Lee <112963362+Hbyien@users.noreply.github.com>
2024-10-16 22:42:43 +0900

    idle 애니메이션 구현, 오른쪽 왼쪽 이동시 애니메이션 반대로

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from pico2d import *
 
 open_canvas()
 character_walk = load_image('image//walk.png')
+character_idle = load_image('image//idle.png')
 
 
 
 def handle_events():
     global running
     global  dir
+    global look_right, stop
+
 
     events = get_events()
     for event in events:
@@ -21,30 +24,61 @@ def handle_events():
 
             elif event.key == SDLK_d:  #오른쪽이동
                 dir +=1
+                stop = False
+                look_right = True
             elif event.key == SDLK_a: #왼쪽이동
                 dir -=1
+                stop = False
+                look_right = False
 
         elif event.type ==SDL_KEYUP:
-            if event.key == SDLK_d:  #오른쪽이동
+            if event.key == SDLK_d:  #오른쪽보고 멈춤
                 dir -=1
-            elif event.key == SDLK_a: #왼쪽이동
+                stop = True
+                look_right = True
+            elif event.key == SDLK_a: #왼쪽보고 멈춤
                 dir +=1
+                stop = True
+                look_right = False
 
 #--------------------------------
 frame = 0
 running  = True
 x = 800//2
 dir = 0
+look_right = True
+stop = True
+
+#---------------------------------
 
 while running:
     clear_canvas()
-    character_walk.clip_draw(frame*96, 0, 96, 94, x, 90)
-    update_canvas()
+    if stop == True:
+        if look_right == True:
+            character_idle.clip_draw(frame *94, 0, 94, 101, x, 90)
+        elif look_right == False:
+            character_idle.clip_composite_draw(frame * 94, 0, 94, 101,  0, 'h', x, 90, 100, 100)
 
+        frame = (frame+1) %4
+
+    elif stop == False:
+        if look_right == True:
+            character_walk.clip_draw(frame *96, 0, 96, 94, x, 90)
+        elif look_right == False:
+            character_walk.clip_composite_draw(frame * 96, 0, 96, 94,0, 'h', x, 90, 100, 100)
+
+        frame = (frame + 1) % 6
+
+    update_canvas()
     handle_events()
 
-    frame = (frame+1) %6
+
     x += dir*10
+    if x>790:
+        x -= 10
+    elif x<10:
+        x+=10
+
     delay(0.05)
 
 close_canvas()
