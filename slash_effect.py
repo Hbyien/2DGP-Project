96e82e79e9d99ca114aa9eed42770f9d2b23a5e7
Hyebin <112963362+Hbyien@users.noreply.github.com>
2024-11-17 21:14:50 +0900

    슬래시 수정

--- slash_effect.py
+++ slash_effect.py
@@ -1,6 +1,7 @@
 from pico2d import load_image
 
 import game_world
+import game_framework
 
 
 class Slash_Effect:
@@ -11,13 +12,18 @@ class Slash_Effect:
             Slash_Effect.image = load_image('image//slash_effect.png')
 
         self.x, self.y , self.velocity = x, y, velocity
+        self.knight_x = x
 
     def draw(self):
         if self.velocity>=0:
-            self.image.clip_draw(0,0 ,190,144,self.x, self.y)
+            self.image.clip_draw(0,0 ,190,144,self.x, self.y, 100, 100)
         else:
-            self.image.clip_composite_draw(0, 0, 190, 144, 0, 'h', self.x, self.y, 190, 144)
+            self.image.clip_composite_draw(0, 0, 190, 144, 0, 'h', self.x, self.y, 100, 100)
 
     def update(self):
 
-        self.x += self.velocity
+        self.x += self.velocity*100*game_framework.frame_time
+
+        if abs(self.x - self.knight_x) > 100:
+            game_world.remove_object(self)
+
