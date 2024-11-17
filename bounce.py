e2e9e140fa40ea559ade56d4003de17ce670ca28
Hyebin <112963362+Hbyien@users.noreply.github.com>
2024-11-18 02:57:56 +0900

    리듬 바운스 수정

--- bounce.py
+++ bounce.py
@@ -1,4 +1,5 @@
 from pico2d import load_image
+import rhythm_bar
 
 Map_Width, Map_Height = 1200, 600
 class Bounce:
@@ -8,6 +9,8 @@ class Bounce:
 
     def draw(self):
         self.image.clip_draw(0, 0, 161, 166 ,600, 100, 60, 60)
+        if rhythm_bar.Rhythm_Bar.rhythm_perfect == True:
+            self.blue_image.clip_draw(0, 0, 161, 166, 600, 100, 60, 60)
 
     def update(self):
         pass
--- rhythm_bar.py
+++ rhythm_bar.py
@@ -12,19 +12,23 @@ class Rhythm_Bar:
 
         self.x1 = 20
         self.velocity = 3  # 속도 설정
+        self.x2 = 1180
 
     def draw(self):
-        self.image.clip_draw(0, 0, 190, 144, self.x1, 20, 100, 100)
+        self.image.clip_draw(0, 0, 190, 144, self.x1, 60, 100, 100)
+        self.image.clip_draw(0, 0, 190, 144, self.x2, 60, 100, 100)
 
     def update(self):
         # 리듬 바의 위치 업데이트
         self.x1 += self.velocity * 100 * game_framework.frame_time
+        self.x2 -= self.velocity * 100 * game_framework.frame_time
 
         # 리듬바가 화면 밖으로 나가면 초기 위치로 재설정
-        if  self.x1>=500:
+        if  self.x1>=550:
             Rhythm_Bar.rhythm_perfect = True
               # 600에 도달했음을 표시
         elif self.x1< 500:
             Rhythm_Bar.rhythm_perfect = False  # 아직 도달하지 않음
         if self.x1 >= 600:
             self.x1 = 20  # 초기 위치로 재설정
+            self.x2 = 1180
