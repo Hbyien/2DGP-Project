cdcdf1142098fb85f7284d3c90f23959c84ca22b
Hyebin <112963362+Hbyien@users.noreply.github.com>
2024-11-18 22:27:47 +0900

    몬스터 죽는거 추가

--- wmonster.py
+++ wmonster.py
@@ -28,8 +28,32 @@ class Wmonster:
         self.frame = 0
         self.dir =1
         self.frame_width, self.frame_height = 372//4, 80
+        self.die_frame_width, self.die_frame_height = 507//5, 91
+        self.dying = False
+        self.die_frame = 0
+        self.die_frame_time = 0.0
+        self.extra_time = 1.0
+        self.is_extra_time_active = False
 
     def update(self):
+        if self.dying:
+            self.die_frame_time += game_framework.frame_time
+            if not self.is_extra_time_active:  # extra_time 시작 전 프레임 관리
+                if self.die_frame_time >= 0.1:  # 각 프레임의 지속 시간: 0.1초
+                    self.die_frame += 1
+                    self.die_frame_time = 0.0
+
+                if self.die_frame >= 5:  # 모든 프레임이 재생되면 extra_time 활성화
+                    self.is_extra_time_active = True
+                    self.die_frame = 4  # 마지막 프레임을 유지
+
+                # extra_time 감소
+            if self.is_extra_time_active:
+                self.extra_time -= game_framework.frame_time
+                if self.extra_time <= 0:
+                    game_world.remove_object(self)
+            return
+
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
         self.x += RUN_SPEED_PPS * self.dir * game_framework.frame_time
         if self.x > 1100:
@@ -40,12 +64,19 @@ class Wmonster:
         pass
 
     def draw(self):
-        if self.dir==-1:
-            self.walk_image.clip_composite_draw(int(self.frame) * self.frame_width, 0, self.frame_width, self.frame_height,0, 'h', self.x, self.y, 90, 90)
-        elif self.dir ==1:
-            self.walk_image.clip_draw(int(self.frame)* self.frame_width, 0, self.frame_width, self.frame_height, self.x, self.y)
+        if self.dying:  # 죽는 상태에서는 die_image를 그림
+            if self.die_frame < 5:  # die_image의 프레임 재생
+                if self.dir == 1:
+                    self.die_image.clip_draw(self.die_frame * self.die_frame_width, 0, self.die_frame_width, self.die_frame_height, self.x, self.y)
+                elif self.dir == -1:
+                    self.die_image.clip_composite_draw(self.die_frame * self.die_frame_width, 0, self.die_frame_width, self.die_frame_height, 0, 'h', self.x, self.y, 90, 90 )
+        else :
+            if self.dir==-1:
+                self.walk_image.clip_composite_draw(int(self.frame) * self.frame_width, 0, self.frame_width, self.frame_height,0, 'h', self.x, self.y, 90, 90)
+            elif self.dir ==1:
+                self.walk_image.clip_draw(int(self.frame)* self.frame_width, 0, self.frame_width, self.frame_height, self.x, self.y)
 
-        draw_rectangle(*self.get_bb())
+            draw_rectangle(*self.get_bb())
 
     def handle_event(self, event):
         pass
@@ -58,7 +89,7 @@ class Wmonster:
 
     def handle_collision(self, group, other):
         if group == 'slash_effect:wmonster':
-            game_world.remove_object(self)
+            self.dying  = True
 
         if group =='knight: wmonster':
             game_framework.quit()
